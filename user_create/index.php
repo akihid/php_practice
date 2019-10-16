aa350bc8f4d39579407cbc5a8196dd4f0e7a0dc1
sato akihide <akihide315@gmail.com>
2019-10-16 16:40:43 +0900

    Add: ユーザー登録画面、マイページ画面作成

---
+++ user_create/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>mypage</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>ユーザー登録</h1>
+    <form action="" method="post">
+      <input type="text" name="email" placeholder="email">
+      <input type="text" name="pass" placeholder="パスワード">
+      <input type="text" name="pass_retype" placeholder="パスワード（再入力）">
+      <input type="submit" value="送信">
+    </form>
+    <a href="mypage.php">マイページへ</a>
+  </body>
+</html>
---
+++ user_create/mypage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>mypage</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>マイページ</h1>
+    <section>
+      <p>
+        あなたのemailはinfo@test.comです<br/>
+        あなたのpassは passwordです。
+      </p>
+    </section>
+    <a href="index.php">ユーザー登録画面へ</a>
+  </body>
+</html>
