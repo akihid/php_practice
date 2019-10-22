b4217af2d1d680e9436ee1cf0e1dc316a8c65445
sato akihide <akihide315@gmail.com>
2019-10-22 13:55:49 +0900

    Add: 画像のバリデーション簡易版作成

--- upload/upload.php
+++ upload/upload.php
@@ -2,6 +2,21 @@
 
 if(!empty($file)){
   // var_dump($file);
+
+  // バリデーションチェック
+  $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+  var_dump($file['size']);
+  if ($ext == 'jpg' || $ext == 'png') {
+    if ($file['size'] > 100000) {
+      $msg = 'ファイルサイズエラー(100KB以下のみアップ可能)';
+    }
+  } else {
+    $msg = 'jpg,pngのみアップロード可能';
+  }
+  var_dump($msg);
+  // チェックOKのときのみ
+  if(empty($msg)){
+
   // サーバーに画像を保存
   $upload_path = 'images/'.$file['name'];
   $result = move_uploaded_file($file['tmp_name'], $upload_path);
@@ -12,6 +27,7 @@ if(!empty($file)){
     $img_path = $upload_path; 
   }else{
     $msg = '画像アップ失敗.エラー内容：'.$file['error'];
+  }
   }
 } else{
   $msg = '画像を選択してください';
